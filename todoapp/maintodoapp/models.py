2fa3898147d0afb969b4156da8e0239078f0621b
andreireporter13 <andrei.reporter13@gmail.com>
2023-07-17 01:45:11 +0300

    change to db

--- todoapp/maintodoapp/models.py
+++ todoapp/maintodoapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Tasks(models.Model):
+    task = models.CharField(max_length=100)
+    done_task = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.task} <---> {self.done_task}'
--- todoapp/maintodoapp/views.py
+++ todoapp/maintodoapp/views.py
@@ -2,35 +2,12 @@
 #
 #
 from django.shortcuts import render
-
-
-# data --->
-tasks = [
-        {'id': 1,
-         'task': 'Sa merg la mare',
-         'status_task': True
-         },
-
-        {'id': 2,
-         'task': 'Sa cumpar paine',
-         'status_task': False
-         },
-
-        {'id': 3,
-         'task': 'Sa citesc',
-         'status_task': False
-         },
-
-        {'id': 4,
-         'task': 'Sa fac meditatii',
-         'status_task': True
-         }
-    ]
+from .models import Tasks
 
 
 def home(request):
-    context = {'tasks': tasks}
-    return render(request, 'maintodoapp/home.html', context)
+    tasks = Tasks.objects.all()
+    return render(request, 'maintodoapp/home.html', {'tasks': tasks})
 
 
 def about(request):
