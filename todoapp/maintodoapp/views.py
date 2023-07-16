4a59d87b2808d12a3515d4589691f1f4799ca5d1
andreireporter13 <andrei.reporter13@gmail.com>
2023-07-16 23:38:48 +0300

    refactor html, add bootstrap

--- todoapp/maintodoapp/views.py
+++ todoapp/maintodoapp/views.py
@@ -4,8 +4,33 @@
 from django.shortcuts import render
 
 
+# data --->
+tasks = [
+        {'id': 1,
+         'task': 'Sa merg la mare',
+         'status_task': True
+         },
+
+        {'id': 2,
+         'task': 'Sa cumpar paine',
+         'status_task': False
+         },
+
+        {'id': 3,
+         'task': 'Sa citesc',
+         'status_task': False
+         },
+
+        {'id': 4,
+         'task': 'Sa fac meditatii',
+         'status_task': True
+         }
+    ]
+
+
 def home(request):
-    return render(request, 'maintodoapp/home.html')
+    context = {'tasks': tasks}
+    return render(request, 'maintodoapp/home.html', context)
 
 
 def about(request):
